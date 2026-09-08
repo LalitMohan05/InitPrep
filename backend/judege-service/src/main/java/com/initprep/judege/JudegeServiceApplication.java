f6e41e972c818250e9586af2bd344388b576d45c
lalitMohan05 <lalitmishra2972005@gmail.com>
2026-09-08 18:46:02 +0530

    attempt-service config changes

---
+++ backend/judege-service/src/main/java/com/initprep/judege/JudegeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.initprep.judege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudegeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudegeServiceApplication.class, args);
+	}
+
+}
---
+++ backend/judege-service/src/test/java/com/initprep/judege/JudegeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.initprep.judege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudegeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
